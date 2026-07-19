d2ea42f0d2f9c7aa5a20c3afa112de5219392ab3
dam107073 <dam107073@gmail.com>
2026-07-18 22:15:44 -0400

    update app versions

--- www/js/constants.js
+++ www/js/constants.js
@@ -2,8 +2,8 @@
 // APP CONFIG
 // ========================================
 
-const APP_VERSION = "1.3.2";
-    const BUILD_DATE = "2026-07-14";
+const APP_VERSION = "1.3.3";
+    const BUILD_DATE = "2026-07-18";
 
     const REVERSE_FLOW_PRO_PRODUCT_ID =
 	  "reverse_flow_pro_lifetime";
